b233888ce8fe8316618e00feae612bdf0f2fd389
Skoatpalace <richard.fages@gmail.com>
2020-03-09 15:47:50 +0100

    goroutines, channels & results

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"time"
 	"training.go/imgproc/task"
 	"training.go/imgproc/filter"
 )
 
 func main()  {
 	var f filter.Filter = filter.Grayscale{}
-	t := task.NewWaitGrpTask("./imgs", "output", f)
+	//t := task.NewWaitGrpTask("./imgs", "output", f)
+	t := task.NewChanTask("./imgs", "output", f, 16)
+
+	start := time.Now()
 	t.Process()
+	elapsed := time.Since(start)
+	fmt.Printf("Image processing took %s\n", elapsed)
 }
